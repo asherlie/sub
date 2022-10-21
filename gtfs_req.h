f1e502c923ea4b0abc7cf826064155e03e9741e1
asherlie <lieber.asher@gmail.com>
2022-10-21 18:38:06 -0400

    added gtfs_req.h

---
+++ gtfs_req.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <curl/curl.h>
+
+enum train{
+    ACE = 0,
+    BDFM,
+    G,
+    JZ,
+    NQRW,
+    L,
+    NUMBERS
+};
+
+const char* url_lookup[] = {"-ace", "-bdfm", "-g", "-jz", "-nqrw", "-l", ""};
+const int url_lookup_len[] = {4, 5, 2, 3, 5, 2, 0};
+
+struct mta_req{
+    struct curl_slist* hdr;
+    CURL* curl;
+};
+
+struct mta_req* setup_mr();
+void cleanup_mr(struct mta_req* mr);
+uint8_t* curl_request(CURL* curl, char* url, int* len, CURLcode* res);
+
