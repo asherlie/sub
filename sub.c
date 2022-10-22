def527679d77b374d8ec44b59a2a1324d33c7e91
asherlie <lieber.asher@gmail.com>
2022-10-22 16:03:11 -0400

    more useful output from sub

--- sub.c
+++ sub.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "stopmap.h"
 #include "gtfs_req.h"
@@ -17,17 +18,27 @@ void pb_free(void* alloc_data, void* ptr){
     free(ptr);
 }
 
-int main(){
 /*
- *     struct TransitRealtime__VehiclePosition vpos;
- * 
- *     transit_realtime__vehicle_position__init(&vpos);
- *     transit_realtime__vehicle_position_un
-*/
+ * unmarked: 0
+ * northbound: 1
+ * southbound: 2
+ */
+int train_direction(char* stop_id){
+    switch(stop_id[strlen(stop_id)-1]){
+        case 'N':
+            return 1;
+        case 'S':
+            return 2;
+    }
+    return 0;
+}
+
+int main(){
     struct mta_req* mr = setup_mr();
 
     struct stopmap sm;
     FILE* fp;
+    time_t cur_time;
 
     uint8_t* data;
     int len;
@@ -43,23 +54,19 @@ int main(){
     build_stopmap(&sm, fp);
     fclose(fp);
 
-    data = mta_request(mr, BDFM, &len, &res);
-    puts((char*)data);
-    printf("read %i bytes, result: %i\n", len, res);
+    data = mta_request(mr, NUMBERS, &len, &res);
+    cur_time = time(NULL);
 
     feedmsg = transit_realtime__feed_message__unpack(&allocator, len, data);
-    #if !1
-    feedmsg->n_entity;
-    feedmsg->entity->trip_update[0];
-    feedmsg->entity->trip_update[0]->n_stop_time_update;
-    feedmsg->entity->trip_update[0]->stop_time_update[0]->departure->time;
-    feedmsg->entity->trip_update[0]->trip->base;
-    #endif
+    /*
+     * we can print N upcoming trains based on responses, they're sorted by arrival time
+    */
+    char* train_name = NULL;
     if(feedmsg){
         for(size_t i = 0; i < feedmsg->n_entity; ++i){
             /*printf("vehicle label: %s\n", feedmsg->entity[i]->vehicle->vehicle->label);*/
             TransitRealtime__FeedEntity* e = feedmsg->entity[i];
-            if(e->vehicle)printf("train: %s\n", feedmsg->entity[i]->vehicle->trip->route_id);
+            if(e->vehicle)train_name = e->vehicle->trip->route_id;
             if(!e->trip_update)continue;
             for(size_t j = 0; j < e->trip_update->n_stop_time_update; ++j){
                 struct TransitRealtime__TripUpdate__StopTimeUpdate* stu;
@@ -69,7 +76,13 @@ int main(){
 /*printf("label: %s\n", feedmsg->entity[i]->trip_update->stop_time_update[j]->*/
                 if(!stu->arrival)continue;
                 t = stu->arrival->time;
-                printf("arriving to stop: %s at: %s\n", lookup_stopmap(&sm, stu->stop_id), ctime(&t));
+                 /*
+                  * printf("%s train arriving to stop: %s at: %s\n", train_name, lookup_stopmap(&sm, stu->stop_id), ctime(&t));
+                  * printf("%s train arriving to stop: %s at: %li\n", train_name, lookup_stopmap(&sm, stu->stop_id), t-cur_time);
+                 */
+                 printf("%sbound %s train arriving at stop: %s in: %li seconds\n",
+                       (train_direction(stu->stop_id) == 1) ? "north" : "south",
+                       train_name, lookup_stopmap(&sm, stu->stop_id), t-cur_time);
             }
         }
     }
