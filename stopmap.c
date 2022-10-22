0e7d0f2d1be696a1a8c390e9392eb8af196fa260
asherlie <lieber.asher@gmail.com>
2022-10-22 02:19:28 -0400

    stubbed out stopmap code

--- stopmap.c
+++ stopmap.c
@@ -1,3 +1,31 @@
-#include "stopmap.h"
+#include <stdlib.h>
 
-void init_stopmap(struct stopmap* sm, int n_buckets);
+#include "stopmap.h"
+// sm should be generated and stored on disk to be quickly
+// loaded back
+
+void init_stopmap(struct stopmap* sm, int n_buckets){
+    sm->n_buckets = n_buckets;
+    sm->buckets = calloc(sizeof(struct stop*), n_buckets);
+}
+
+void free_stopmap(struct stopmap* sm){
+    for(int i = 0; i < sm->n_buckets; ++i){
+        if(sm->buckets[i])free(sm->buckets[i]);
+    }
+    free(sm->buckets);
+}
+
+void insert_stopmap(struct stopmap* sm, char* stop_id, char* stop_name){
+}
+
+char* lookup_stopmap(struct stopmap* sm, char* stop_id){
+}
+
+int main(){
+    struct stopmap sm;
+    init_stopmap(&sm, 1000);
+    insert_stopmap(&sm, "J14N", "104 St");
+    insert_stopmap(&sm, "103N", "238 St");
+    free_stopmap(&sm);
+}
